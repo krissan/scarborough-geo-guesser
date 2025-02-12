2ecf80da51258709d5eaf25f0a2d910d49cac741
AkashSivapalan <akashsivapalan@gmail.com>
2025-02-11 22:38:53 -0500

    Added highscore ui update

--- src/app/sections/HighScores.tsx
+++ src/app/sections/HighScores.tsx
@@ -9,13 +9,16 @@ interface HighScoresProps {
 // Landing Screen for website
 const HighScores: React.FC<HighScoresProps> = ({ highScores, loading }) => {
   return (loading ? <div className=""></div> :
-    <div className="flex flex-col items-center sm:items-start">
+    <div className="flex flex-col items-center sm:items-start mb-2.5">
     <SubTitle>High Scores</SubTitle>
-    <div className="flex flex-col items-center sm:items-start">
+    <div className="flex flex-col items-center sm:items-start ">
       {highScores.map((score, index) => (
-        <div key={index} className="flex flex-row justify-between w-full">
-          <p>{score.playerName}</p>
-          <p>{score.score}</p>
+          <div
+          key={index}
+          className="flex flex-row justify-between w-auto px-4 py-2 rounded-lg bg-gray-100 shadow-md  border border-black"
+        >
+          <p className="font-semibold text-lg text-gray-800">{score.playerName}:</p>
+          <p className="font-bold text-lg text-blue-600">{score.score}</p>
         </div>
       ))}
     </div>
